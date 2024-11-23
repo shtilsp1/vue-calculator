68723c0885155f9191d713334b9acb7da30c0422
shtilsp1 <shtilsp1@mail.ru>
2024-11-23 16:47:24 +0300

    Add calculator interface

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,28 @@
 
 <template>
+  <div class="calculator">
+    <div class="screen">{{ display }}</div>
+    <div class="buttons">
+      <button @click="appendNumber('1')">1</button>
+      <button @click="appendNumber('2')">2</button>
+      <button @click="appendNumber('3')">3</button>
+      <button @click="appendNumber('4')">4</button>
+      <button @click="appendNumber('5')">5</button>
+      <button @click="appendNumber('6')">6</button>
+      <button @click="appendNumber('7')">7</button>
+      <button @click="appendNumber('8')">8</button>
+      <button @click="appendNumber('9')">9</button>
+      <button @click="appendNumber('0')">0</button>
 
+      <button @click="chooseOperation('+')">+</button>
+      <button @click="chooseOperation('-')">-</button>
+      <button @click="chooseOperation('*')">*</button>
+      <button @click="chooseOperation('/')">/</button>
+
+      <button @click="clear">C</button>
+      <button @click="calculate">=</button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
